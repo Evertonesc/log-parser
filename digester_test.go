37264c6279a10bbc548293dbd35140fcdf77415d
esc <evertonsouzaesc@gmail.com>
2024-07-13 16:02:19 -0300

    test: tweak and add unit tests

--- digester_test.go
+++ digester_test.go
@@ -24,20 +24,64 @@ func TestLogsDigester(t *testing.T) {
 			},
 			wantErr: nil,
 			wantMatch: &Match{
-				Players: []string{},
+				TotalKills:   0,
+				Players:      []string{},
+				Kills:        map[string]int{},
+				KillsByMeans: map[string]int{},
+				Done:         false,
+				InProgress:   true,
 			},
 		},
 		{
 			name: "should parse the client user info changed log entry adding a new player to the match",
 			args: args{
 				logLine: " 20:34 ClientUserinfoChanged: 2 n\\Isgalamido\\t\\0\\model\\xian/default\\hmodel\\xian/default\\g_redteam\\\\g_blueteam\\\\c1\\4\\c2\\5\\hc\\100\\w\\0\\l\\0\\tt\\0\\tl\\0",
-				match:   &Match{},
+				match: &Match{
+					TotalKills:   0,
+					Players:      make([]string, 0),
+					Kills:        map[string]int{},
+					KillsByMeans: map[string]int{},
+					InProgress:   true,
+					Done:         false,
+				},
 			},
 			wantMatch: &Match{
 				Players: []string{"Isgalamido"},
 				Kills: map[string]int{
 					"Isgalamido": 0,
 				},
+				KillsByMeans: map[string]int{},
+				InProgress:   true,
+			},
+			wantErr: nil,
+		},
+		{
+			name: "should parse the kill log line and add the details to the match info",
+			args: args{
+				logLine: " 20:54 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT",
+				match: &Match{
+
+					TotalKills: 0,
+					Players:    []string{"Isgalamido"},
+					Kills: map[string]int{
+						"Isgalamido": 1,
+					},
+					KillsByMeans: map[string]int{},
+					Done:         false,
+					InProgress:   true,
+				},
+			},
+			wantMatch: &Match{
+				TotalKills: 1,
+				Players:    []string{"Isgalamido"},
+				Kills: map[string]int{
+					"Isgalamido": 0,
+				},
+				KillsByMeans: map[string]int{
+					"MOD_TRIGGER_HURT": 1,
+				},
+				Done:       false,
+				InProgress: true,
 			},
 			wantErr: nil,
 		},
@@ -54,6 +98,9 @@ func TestLogsDigester(t *testing.T) {
 				}
 			}
 
+			assert.Equal(t, tt.wantMatch.TotalKills, tt.args.match.TotalKills)
+			assert.Equal(t, tt.wantMatch.InProgress, tt.args.match.InProgress)
+			assert.Equal(t, tt.wantMatch.Done, tt.args.match.Done)
 			assert.Equal(t, len(tt.wantMatch.Players), len(tt.args.match.Players))
 
 			for wantPlayer, wantKillValue := range tt.wantMatch.Kills {
