20974ccbdeac452d1182169df878346f5cd03d5c
esc <evertonsouzaesc@gmail.com>
2024-07-13 16:00:37 -0300

    feat: implement a method to store kills and kills by means data to the match

--- match.go
+++ match.go
@@ -1,5 +1,9 @@
 package main
 
+const (
+	world = "<world>"
+)
+
 type Match struct {
 	TotalKills   int            `json:"total_kills"`
 	Players      []string       `json:"players"`
@@ -23,4 +27,15 @@ func (m *Match) AddKillStats(player string) {
 	m.Kills = map[string]int{
 		player: 0,
 	}
+}
+
+func (m *Match) AddKillAndMeans(killer, killed, reason string) {
+	if killer == world {
+		m.Kills[killed]--
+	} else {
+		m.Kills[killer]++
+	}
+
+	m.KillsByMeans[reason]++
+	m.TotalKills++
 }
---
+++ match_test.go
@@ -0,0 +1,95 @@
+package main
+
+import "testing"
+
+func TestMatch_AddKillAndMeans(t *testing.T) {
+	type fields struct {
+		TotalKills   int
+		Players      []string
+		Kills        map[string]int
+		KillsByMeans map[string]int
+		Done         bool
+	}
+	type args struct {
+		killer string
+		killed string
+		reason string
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		args      args
+		wantMatch *Match
+	}{
+		{
+			name: "should insert the kill information to the match",
+			fields: fields{
+				TotalKills: 0,
+				Players:    []string{"Isgalamido", "Bruce Wayne"},
+				Kills: map[string]int{
+					"Isgalamido":  0,
+					"Bruce Wayne": 0,
+				},
+			},
+			args: args{
+				killer: "Bruce Wayne",
+				killed: "Isgalamido",
+				reason: "MOD_ROCKET_SPLASH",
+			},
+			wantMatch: &Match{
+				TotalKills: 1,
+				Players:    []string{"Isgalamido", "Bruce Wayne"},
+				Kills: map[string]int{
+					"Isgalamido":  0,
+					"Bruce Wayne": 1,
+				},
+				KillsByMeans: map[string]int{
+					"MOD_ROCKET_SPLASH": 1,
+				},
+			},
+		},
+		{
+			name: "should insert the kill information and decrease the killed player by world",
+			fields: fields{
+				TotalKills: 10,
+				Players:    []string{"Isgalamido", "Bruce Wayne"},
+				Kills: map[string]int{
+					"Isgalamido":  8,
+					"Bruce Wayne": 2,
+				},
+				KillsByMeans: map[string]int{
+					"MOD_ROCKET_SPLASH": 1,
+				},
+			},
+			args: args{
+				killer: "<world>",
+				killed: "Isgalamido",
+				reason: "MOD_TRIGGER_HURT",
+			},
+			wantMatch: &Match{
+				TotalKills: 11,
+				Players:    []string{"Isgalamido", "Bruce Wayne"},
+				Kills: map[string]int{
+					"Isgalamido":  7,
+					"Bruce Wayne": 2,
+				},
+				KillsByMeans: map[string]int{
+					"MOD_ROCKET_SPLASH": 1,
+					"MOD_TRIGGER_HURT":  1,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Match{
+				TotalKills:   tt.fields.TotalKills,
+				Players:      tt.fields.Players,
+				Kills:        tt.fields.Kills,
+				KillsByMeans: tt.fields.KillsByMeans,
+				Done:         tt.fields.Done,
+			}
+			m.AddKillAndMeans(tt.args.killer, tt.args.killed, tt.args.reason)
+		})
+	}
+}
