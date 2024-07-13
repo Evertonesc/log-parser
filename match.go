f3fa2c980e149c6b0af155fb8bc3485284ea1200
esc <evertonsouzaesc@gmail.com>
2024-07-13 15:59:58 -0300

    refactor: initialize all match fields in the factory method and add InProgress control field

--- match.go
+++ match.go
@@ -1,25 +1,25 @@
 package main
 
 type Match struct {
-	TotalKills int            `json:"total_kills"`
-	Players    []string       `json:"players"`
-	Kills      map[string]int `json:"kills"`
-	Done       bool
+	TotalKills   int            `json:"total_kills"`
+	Players      []string       `json:"players"`
+	Kills        map[string]int `json:"kills"`
+	KillsByMeans map[string]int `json:"kills_by_means"`
+	Done         bool
+	InProgress   bool
 }
 
 func NewMatch() *Match {
-	return &Match{}
-}
-
-func (m *Match) InitPlayers() {
-	m.Players = make([]string, 0)
+	return &Match{
+		TotalKills:   0,
+		Players:      make([]string, 0),
+		Kills:        map[string]int{},
+		KillsByMeans: map[string]int{},
+		Done:         false,
+	}
 }
 
 func (m *Match) AddKillStats(player string) {
-	if m.Kills == nil {
-		m.Kills = make(map[string]int)
-	}
-
 	m.Kills = map[string]int{
 		player: 0,
 	}
