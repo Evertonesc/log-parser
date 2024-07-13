516b62fe6d98f35b535a33a5f6b7476647ae8a9f
esc <evertonsouzaesc@gmail.com>
2024-07-13 11:03:21 -0300

    feat: implement methods to match entity

--- match.go
+++ match.go
@@ -1,20 +1,26 @@
 package main
 
 type Match struct {
-	TotalKills int      `json:"total_kills"`
-	Players    []string `json:"players"`
-	Kills      any      `json:"kills"`
+	TotalKills int            `json:"total_kills"`
+	Players    []string       `json:"players"`
+	Kills      map[string]int `json:"kills"`
 	Done       bool
 }
 
 func NewMatch() *Match {
-	return &Match{
-		TotalKills: 0,
-		Players:    make([]string, 0),
-		Kills:      nil,
-	}
+	return &Match{}
+}
+
+func (m *Match) InitPlayers() {
+	m.Players = make([]string, 0)
 }
 
 func (m *Match) AddKillStats(player string) {
+	if m.Kills == nil {
+		m.Kills = make(map[string]int)
+	}
 
+	m.Kills = map[string]int{
+		player: 0,
+	}
 }
