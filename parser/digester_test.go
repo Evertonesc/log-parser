763cd6c8d65d51e1b9f005e8a26504d4fa04d1ca
esc <evertonsouzaesc@gmail.com>
2024-07-15 12:05:41 -0300

    refactor: make a specific unit tests switch for each handler

--- parser/digester_test.go
+++ parser/digester_test.go
@@ -1,12 +1,186 @@
 package parser
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	match "log-parser/match"
 	"testing"
 )
 
-func TestLogsDigester(t *testing.T) {
+func TestInitGameHandler_Handle(t *testing.T) {
+	type args struct {
+		logLine string
+		match   *match.Match
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "should successfully parse a init game log entry",
+			args: args{
+				logLine: "0:00 InitGame: \\\\\\\\sv_floodProtect\\\\\\\\1\\\\\\\\sv_maxPing\\\\\\\\0\\\\\\\\sv_minPing\\\\\\\\0\\\\\\\\sv_maxRate\\\\\\\\10000\\\\\\\\sv_minRate\\\\\\\\0\\\\\\\\sv_hostname\\\\\\\\Code Miner Server\\\\\\\\g_gametype\\\\\\\\0\\\\\\\\sv_privateClients\\\\\\\\2\\\\\\\\sv_maxclients\\\\\\\\16\\\\\\\\sv_allowDownload\\\\\\\\0\\\\\\\\dmflags\\\\\\\\0\\\\\\\\fraglimit\\\\\\\\20\\\\\\\\timelimit\\\\\\\\15\\\\\\\\g_maxGameClients\\\\\\\\0\\\\\\\\capturelimit\\\\\\\\8\\\\\\\\version\\\\\\\\ioq3 1.36 linux-x86_64 Apr 12 2009\\\\\\\\protocol\\\\\\\\68\\\\\\\\mapname\\\\\\\\q3dm17\\\\\\\\gamename\\\\\\\\baseq3\\\\\\\\g_needpass\\\\\\\\0\\n",
+				match:   match.NewMatch(),
+			},
+			wantErr: assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := NewInitGameHandler()
+			tt.wantErr(t, h.Handle(tt.args.logLine, tt.args.match), fmt.Sprintf("Handle(%v, %v)", tt.args.logLine, tt.args.match))
+
+			m := tt.args.match
+
+			assert.Equal(t, true, m.InProgress)
+		})
+	}
+}
+
+func TestAddPlayerHandler_Handle(t *testing.T) {
+	type args struct {
+		logLine string
+		match   func() *match.Match
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantMatch *match.Match
+		wantErr   assert.ErrorAssertionFunc
+	}{
+		{
+			name: "should successfully parse a client user info changed log entry and update the match data with a new player",
+			args: args{
+				logLine: " 20:34 ClientUserinfoChanged: 2 n\\Isgalamido\\t\\0\\model\\xian/default\\hmodel\\xian/default\\g_redteam\\\\g_blueteam\\\\c1\\4\\c2\\5\\hc\\100\\w\\0\\l\\0\\tt\\0\\tl\\0",
+				match: func() *match.Match {
+					m := match.NewMatch()
+					m.InProgress = true
+
+					return m
+				},
+			},
+			wantMatch: &match.Match{
+				TotalKills: 0,
+				Players:    []string{"Isgalamido"},
+				Kills: map[string]int{
+					"Isgalamido": 0,
+				},
+				KillsByMeans: nil,
+				PlayersInGame: map[string]bool{
+					"Isgalamido": true,
+				},
+				Done:       false,
+				InProgress: true,
+			},
+			wantErr: assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := NewAddPlayerHandler()
+			m := tt.args.match()
+			tt.wantErr(t, h.Handle(tt.args.logLine, m), fmt.Sprintf("Handle(%v, %v)", tt.args.logLine, m))
+
+			assert.Equal(t, len(tt.wantMatch.Players), len(m.Players))
+			assert.Equal(t, tt.wantMatch.Players[0], m.Players[0])
+
+			for k, v := range tt.wantMatch.Kills {
+				assert.Equal(t, v, m.Kills[k])
+			}
+
+			for k, v := range tt.wantMatch.PlayersInGame {
+				assert.Equal(t, v, m.PlayersInGame[k])
+			}
+		})
+	}
+}
+
+func TestKillDetailsHandler_Handle(t *testing.T) {
+	type args struct {
+		logLine string
+		match   func() *match.Match
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantMatch *match.Match
+		wantErr   assert.ErrorAssertionFunc
+	}{
+		{
+			name: "should successfully parse the kill log entry and add its information to the match",
+			args: args{
+				logLine: " 20:54 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT",
+				match: func() *match.Match {
+					return &match.Match{
+						TotalKills: 5,
+						Players:    []string{"Isgalamido", "Bruce Wayne"},
+						Kills: map[string]int{
+							"Isgalamido":  3,
+							"Bruce Wayne": 2,
+						},
+						KillsByMeans: map[string]int{
+							"MOD_TRIGGER_HURT":  1,
+							"MOD_ROCKET_SPLASH": 2,
+							"MOD_FALLING":       2,
+						},
+						PlayersInGame: map[string]bool{
+							"Isgalamido":  true,
+							"Bruce Wayne": true,
+						},
+						Done:       false,
+						InProgress: true,
+					}
+
+				},
+			},
+			wantMatch: &match.Match{
+				TotalKills: 6,
+				Players:    []string{"Isgalamido", "Bruce Wayne"},
+				Kills: map[string]int{
+					"Isgalamido":  2,
+					"Bruce Wayne": 2,
+				},
+				KillsByMeans: map[string]int{
+					"MOD_TRIGGER_HURT":  2,
+					"MOD_ROCKET_SPLASH": 2,
+					"MOD_FALLING":       2,
+				},
+				PlayersInGame: map[string]bool{
+					"Isgalamido":  true,
+					"Bruce Wayne": true,
+				},
+				Done:       false,
+				InProgress: true,
+			},
+			wantErr: assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := NewKillDetailsHandler()
+			m := tt.args.match()
+			tt.wantErr(t, h.Handle(tt.args.logLine, m), fmt.Sprintf("Handle(%v, %v)", tt.args.logLine, m))
+
+			assert.Equal(t, len(tt.wantMatch.Players), len(m.Players))
+			assert.Equal(t, tt.wantMatch.Players[0], m.Players[0])
+
+			for k, v := range tt.wantMatch.Kills {
+				assert.Equal(t, v, m.Kills[k])
+			}
+
+			for k, v := range tt.wantMatch.PlayersInGame {
+				assert.Equal(t, v, m.PlayersInGame[k])
+			}
+
+			for k, v := range tt.wantMatch.KillsByMeans {
+				assert.Equal(t, v, m.KillsByMeans[k])
+			}
+		})
+	}
+}
+
+func TestEndGameHandler_Handle(t *testing.T) {
 	type args struct {
 		logLine string
 		match   *match.Match
@@ -14,159 +188,122 @@ func TestLogsDigester(t *testing.T) {
 	tests := []struct {
 		name      string
 		args      args
-		wantErr   error
 		wantMatch *match.Match
+		wantErr   assert.ErrorAssertionFunc
 	}{
 		{
-			name: "should parse the init game log entry",
-			args: args{
-				logLine: "0:00 InitGame: \\\\sv_floodProtect\\\\1\\\\sv_maxPing\\\\0\\\\sv_minPing\\\\0\\\\sv_maxRate\\\\10000\\\\sv_minRate\\\\0\\\\sv_hostname\\\\Code Miner Server\\\\g_gametype\\\\0\\\\sv_privateClients\\\\2\\\\sv_maxclients\\\\16\\\\sv_allowDownload\\\\0\\\\dmflags\\\\0\\\\fraglimit\\\\20\\\\timelimit\\\\15\\\\g_maxGameClients\\\\0\\\\capturelimit\\\\8\\\\version\\\\ioq3 1.36 linux-x86_64 Apr 12 2009\\\\protocol\\\\68\\\\mapname\\\\q3dm17\\\\gamename\\\\baseq3\\\\g_needpass\\\\0\n",
-				match:   &match.Match{},
-			},
-			wantErr: nil,
-			wantMatch: &match.Match{
-				TotalKills:   0,
-				Players:      []string{},
-				Kills:        map[string]int{},
-				KillsByMeans: map[string]int{},
-				Done:         false,
-				InProgress:   true,
-			},
-		},
-		{
-			name: "should parse the client user info changed log entry adding a new player to the match",
-			args: args{
-				logLine: " 20:34 ClientUserinfoChanged: 2 n\\Isgalamido\\t\\0\\model\\xian/default\\hmodel\\xian/default\\g_redteam\\\\g_blueteam\\\\c1\\4\\c2\\5\\hc\\100\\w\\0\\l\\0\\tt\\0\\tl\\0",
-				match: &match.Match{
-					TotalKills:    0,
-					Players:       make([]string, 0),
-					Kills:         map[string]int{},
-					KillsByMeans:  map[string]int{},
-					PlayersInGame: map[string]bool{},
-					InProgress:    true,
-					Done:          false,
-				},
-			},
-			wantMatch: &match.Match{
-				Players: []string{"Isgalamido"},
-				Kills: map[string]int{
-					"Isgalamido": 0,
-				},
-				PlayersInGame: map[string]bool{
-					"Isgalamido": true,
-				},
-				KillsByMeans: map[string]int{},
-				InProgress:   true,
-			},
-			wantErr: nil,
-		},
-		{
-			name: "should parse the kill log line and add the details to the match info",
-			args: args{
-				logLine: " 20:54 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT",
-				match: &match.Match{
-
-					TotalKills: 0,
-					Players:    []string{"Isgalamido"},
-					Kills: map[string]int{
-						"Isgalamido": 1,
-					},
-					KillsByMeans: map[string]int{},
-					Done:         false,
-					InProgress:   true,
-				},
-			},
-			wantMatch: &match.Match{
-				TotalKills: 1,
-				Players:    []string{"Isgalamido"},
-				Kills: map[string]int{
-					"Isgalamido": 0,
-				},
-				KillsByMeans: map[string]int{
-					"MOD_TRIGGER_HURT": 1,
-				},
-				Done:       false,
-				InProgress: true,
-			},
-			wantErr: nil,
-		},
-		{
-			name: "should parse the shutdown game log line and finish the match returning its data",
+			name: "should successfully parse a shutdown game log entry and set the done match stats to true and in progress to false",
 			args: args{
 				logLine: " 20:37 ShutdownGame:",
 				match: &match.Match{
-					TotalKills: 0,
-					Players:    []string{"Isgalamido"},
+					TotalKills: 5,
+					Players:    []string{"Isgalamido", "Bruce Wayne"},
 					Kills: map[string]int{
-						"Isgalamido": 0,
+						"Isgalamido":  3,
+						"Bruce Wayne": 2,
 					},
-					KillsByMeans: map[string]int{},
-					Done:         false,
-					InProgress:   true,
+					KillsByMeans: map[string]int{
+						"MOD_TRIGGER_HURT":  1,
+						"MOD_ROCKET_SPLASH": 2,
+						"MOD_FALLING":       2,
+					},
+					PlayersInGame: map[string]bool{
+						"Isgalamido":  true,
+						"Bruce Wayne": true,
+					},
+					Done:       false,
+					InProgress: true,
 				},
 			},
 			wantMatch: &match.Match{
-				TotalKills: 0,
-				Players:    []string{"Isgalamido"},
+				TotalKills: 5,
+				Players:    []string{"Isgalamido", "Bruce Wayne"},
 				Kills: map[string]int{
-					"Isgalamido": 0,
+					"Isgalamido":  3,
+					"Bruce Wayne": 2,
 				},
-				KillsByMeans: map[string]int{},
-				Done:         true,
-				InProgress:   true,
+				KillsByMeans: map[string]int{
+					"MOD_TRIGGER_HURT":  1,
+					"MOD_ROCKET_SPLASH": 2,
+					"MOD_FALLING":       2,
+				},
+				PlayersInGame: map[string]bool{
+					"Isgalamido":  true,
+					"Bruce Wayne": true,
+				},
+				Done:       true,
+				InProgress: false,
 			},
-			wantErr: nil,
+			wantErr: assert.NoError,
 		},
 		{
-			name: "should parse the end game log line and finish the match returning its data",
+			name: "should successfully parse a log entry that finishes the game by an unknown reason and finish the match",
 			args: args{
 				logLine: "26  0:00 ------------------------------------------------------------",
 				match: &match.Match{
-					TotalKills: 0,
-					Players:    []string{"Isgalamido"},
+					TotalKills: 5,
+					Players:    []string{"Isgalamido", "Bruce Wayne"},
 					Kills: map[string]int{
-						"Isgalamido": 0,
+						"Isgalamido":  3,
+						"Bruce Wayne": 2,
 					},
-					KillsByMeans: map[string]int{},
-					Done:         false,
-					InProgress:   true,
+					KillsByMeans: map[string]int{
+						"MOD_TRIGGER_HURT":  1,
+						"MOD_ROCKET_SPLASH": 2,
+						"MOD_FALLING":       2,
+					},
+					PlayersInGame: map[string]bool{
+						"Isgalamido":  true,
+						"Bruce Wayne": true,
+					},
+					Done:       false,
+					InProgress: true,
 				},
 			},
 			wantMatch: &match.Match{
-				TotalKills: 0,
-				Players:    []string{"Isgalamido"},
+				TotalKills: 5,
+				Players:    []string{"Isgalamido", "Bruce Wayne"},
 				Kills: map[string]int{
-					"Isgalamido": 0,
+					"Isgalamido":  3,
+					"Bruce Wayne": 2,
 				},
-				KillsByMeans: map[string]int{},
-				Done:         true,
-				InProgress:   true,
+				KillsByMeans: map[string]int{
+					"MOD_TRIGGER_HURT":  1,
+					"MOD_ROCKET_SPLASH": 2,
+					"MOD_FALLING":       2,
+				},
+				PlayersInGame: map[string]bool{
+					"Isgalamido":  true,
+					"Bruce Wayne": true,
+				},
+				Done:       true,
+				InProgress: false,
 			},
-			wantErr: nil,
+			wantErr: assert.NoError,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			h := NewEndGameHandler()
+			tt.wantErr(t, h.Handle(tt.args.logLine, tt.args.match), fmt.Sprintf("Handle(%v, %v)", tt.args.logLine, tt.args.match))
 
-			logsDigester := LoadLogsDigester()
-			err := logsDigester.Handle(tt.args.logLine, tt.args.match)
-
-			if tt.wantErr != nil {
-				if err.Error() != tt.wantErr.Error() {
-					t.Errorf("error mismtach, want %s, got %s", tt.wantErr.Error(), err.Error())
-				}
-			}
-
-			assert.Equal(t, tt.wantMatch.TotalKills, tt.args.match.TotalKills)
-			assert.Equal(t, tt.wantMatch.InProgress, tt.args.match.InProgress)
-			assert.Equal(t, tt.wantMatch.Done, tt.args.match.Done)
 			assert.Equal(t, len(tt.wantMatch.Players), len(tt.args.match.Players))
 
-			for wantPlayer, wantKillValue := range tt.wantMatch.Kills {
-				for gotPlayer, gotKillValue := range tt.args.match.Kills {
-					assert.Equal(t, wantPlayer, gotPlayer)
-					assert.Equal(t, wantKillValue, gotKillValue)
-				}
+			wantedPlayersCount := len(tt.wantMatch.Players)
+			for i := 0; i < wantedPlayersCount; i++ {
+				assert.Equal(t, tt.wantMatch.Players[i], tt.args.match.Players[i])
+			}
+
+			for k, v := range tt.wantMatch.Kills {
+				assert.Equal(t, v, tt.args.match.Kills[k])
+			}
+
+			for k, v := range tt.wantMatch.PlayersInGame {
+				assert.Equal(t, v, tt.args.match.PlayersInGame[k])
+			}
+
+			for k, v := range tt.wantMatch.KillsByMeans {
+				assert.Equal(t, v, tt.args.match.KillsByMeans[k])
 			}
 		})
 	}
