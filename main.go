bef1a9a1ca3ae6569cdf8e244da976beea5748f9
esc <evertonsouzaesc@gmail.com>
2024-07-15 21:50:00 -0300

    feat: parse the output data in the main func

--- main.go
+++ main.go
@@ -1,12 +1,62 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"log-parser/match"
+	"log-parser/parser"
+	"time"
+)
+
+const (
+	dateLayout = "02/01/2006 15:04"
 )
 
 func main() {
-	err := parseLog("qgames.log")
+	now := time.Now()
+
+	matches, err := parser.ParseLog("qgames.log")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	n := len(matches)
+
+	games := make([]map[string]*match.Match, n)
+	matchSummary := make([]map[string]match.Summary, n)
+	for i := 0; i < n; i++ {
+		key := fmt.Sprintf("game_%d", i+1)
+
+		games[i] = map[string]*match.Match{
+			key: matches[i],
+		}
+
+		matchSummary[i] = map[string]match.Summary{
+			key: {
+				KillsByMeans: matches[i].KillsByMeans,
+			},
+		}
+	}
+
+	matchesOutput, err := json.MarshalIndent(games, "", "    ")
+	if err != nil {
+		log.Fatalf("marshalling json output: %v", err.Error())
+	}
+
+	summaryOutput, err := json.MarshalIndent(matchSummary, "", "    ")
+	if err != nil {
+		log.Fatalf("marshalling json output: %v", err.Error())
+	}
+
+	reportTime := time.Now().Format(dateLayout)
+
+	fmt.Printf("Matches Report - %v\n", reportTime)
+	fmt.Println(string(matchesOutput))
+
+	fmt.Printf("Matches Summary Report - %v\n", reportTime)
+	fmt.Println(string(summaryOutput))
+
+	elapsedTime := time.Since(now)
+	fmt.Printf("parser taken %v ms to run", elapsedTime.Milliseconds())
 }
